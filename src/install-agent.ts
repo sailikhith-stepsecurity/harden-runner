41e4fbd74f9ba7ec2ee4a8f660926e2d8511e587
sailikhith-stepsecurity <sailikhith@stepsecurity.io>
2026-07-13 10:42:37 +0530

    Update download URL for agent to version 0.0.8

--- src/install-agent.ts
+++ src/install-agent.ts
@@ -225,7 +225,7 @@ export async function installWindowsAgent(configStr: string): Promise<boolean> {
   const agentExePath = path.join(agentDir, "agent.exe");
 
   const downloadPath = await tc.downloadTool(
-    `https://github.com/step-security/agent-int-releases/releases/download/v0.0.7-win-int/agent_windows_amd64.tar.gz `,
+    `https://github.com/step-security/agent-int-releases/releases/download/v0.0.8-win-int/agent_windows_amd64.tar.gz `,
     undefined,
     auth,
   );
